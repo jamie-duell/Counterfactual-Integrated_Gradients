faa1a0a40fbafc1d17dbcf258d64c89512b5d04e
Jamie Duell <jay.duell@hotmail.com>
2023-07-29 02:52:39 +0100

    Update CF-IG.py
    Correct Application for Sigmoid output.

--- CF-IG.py
+++ CF-IG.py
@@ -10,12 +10,12 @@ import matplotlib.pyplot as plt
 
 class CounterfactualIG(ExplainerBase):
 
-    def __init__(self, model_interface, data_interface):
+    def __init__(self, model_interface, data_interface, explain_index):
 
-        super().__init__(model_interface, data_interface)
+        super().__init__(model_interface, data_interface, explain_index)
     
     
-    def generate_nearest_CF_neighbour(self, query_instance, input_index, df_columns_to_drop, categorical_features, ohe=False):
+    def generate_nearest_CF_neighbour(self, query_instance, df_columns_to_drop, categorical_features, ohe=False):
         
         data_interface = self.data_interface
         data_interface = data_interface.drop(labels=df_columns_to_drop, axis=1)
@@ -38,7 +38,7 @@ class CounterfactualIG(ExplainerBase):
         target = np.argmin(cf_pred.detach().numpy())
         dist = distance.squareform(distance.pdist(data_interface))
         nearest_neighbours = np.argsort(dist, axis=1)
-        nearest_neighbours_arr = nearest_neighbours[input_index]
+        nearest_neighbours_arr = nearest_neighbours[self.explain_index]
 
         for i in range(len(nearest_neighbours_arr) - 1):
             model_out_query = self.model_interface(query_instance)
@@ -52,7 +52,7 @@ class CounterfactualIG(ExplainerBase):
         return print("This function is not yet implemented in the non-client side version of this method used in the paper \n simple adaptation of the code in the source file are necessary to make this work in the general case.")
         
     
-    def generate_counterfactuals(self, query_instance, counterfactual, _K=500, decision_boundary_proba=0.5):
+    def generate_counterfactuals(self, query_instance, counterfactual, _K=5000, decision_boundary_proba=0.500000):
        
         """ 
         
@@ -79,31 +79,27 @@ class CounterfactualIG(ExplainerBase):
         # predictions at every step. shape : [n_steps, 1]
         cf_pred = self.model_interface(counterfactual[0])
         target = np.argmax(cf_pred.detach().numpy()) #index returned by arg max of the greatest pred probability aka. if 0 is highest then pred = 0
-        preds = self.model_interface(scaled_features[0])[:, target] 
-    
+        not_target = np.argmax(cf_pred.detach().numpy()) 
+        preds = self.model_interface(scaled_features[0]) #[:, target]
         cf_exp = scaled_features
-    
-        index = min(self.find_indices(self.model_interface(cf_exp[0])[:,target], lambda e: e >= decision_boundary_proba))
+        
+        #for softmax 
+        #index = max(self.find_indices(self.model_interface(cf_exp[0])[:,not_target], lambda e: e < decision_boundary_proba))
+        
+        #update this for < 0.5 = class 0 and >= 0.5 is class 1 
+        if self.model_interface(query_instance[0]) >= decision_boundary_proba: 
+            index = min(self.find_indices(self.model_interface(cf_exp[0]), lambda e: e < decision_boundary_proba))
+        else:
+            index = min(self.find_indices(self.model_interface(cf_exp[0]), lambda e: e > decision_boundary_proba))
     
         new_feature = scaled_features[0][index]
     
-        new_scaled_features = tuple(
-            torch.cat(
-                [baseline + alpha * (input - baseline) for alpha in alphas], dim=0
-            ).requires_grad_()
-            for input, baseline in zip(new_feature, query_instance)
-        )
+        grads = grad(outputs=torch.unbind(preds), inputs=scaled_features)
+        #grads = grad(outputs=torch.unbind(preds2), inputs=new_scaled_features)
     
-        preds2 = self.model_interface(new_scaled_features[0])[:,target]
-    
-        # gradients of predictions wrt input features. shape : [n_steps, n_features]
-    
-        #grads = grad(outputs=torch.unbind(preds), inputs=scaled_features)
-        grads = grad(outputs=torch.unbind(preds2), inputs=new_scaled_features)
-    
-        explanation_a = grads[0].mean(0) #get the mean gradient between both points
+        explanation_a = grads[0][:index].mean(0) #get the mean gradient between both points
         
-        explanation = explanation_a*(counterfactual[0].detach().numpy() - query_instance[0].detach().numpy())
+        explanation = (new_feature.detach().numpy() - query_instance[0].detach().numpy()) *explanation_a.detach().numpy()
             
         return explanation
     
@@ -111,7 +107,7 @@ class CounterfactualIG(ExplainerBase):
         return [i for i, elem in enumerate(lst) if condition(elem)] 
 
     
-    def plot_explanation(self, explanation_array, query_instance, counterfactual):
+    def plot_explanation(self, input_tensor, explanation_array, query_instance, counterfactual):
         """ 
         
         explanation_array = explanation provided by generate_counterfactuals()
@@ -123,10 +119,12 @@ class CounterfactualIG(ExplainerBase):
         thus written as counterfactual_tensor not [counterfactual_tensor]
         
         """
-        explanation = explanation_array.detach().numpy()
+        #explanation = explanation_array.detach().numpy()
         increase_or_decrease_for_counterfactual = query_instance.detach().numpy() - counterfactual.detach().numpy()
         feature_names = self.data_interface.columns.tolist()
         
+        cf_pred = self.model_interface(query_instance)
+        target = np.argmax(cf_pred.detach().numpy())
         
         result_increase_cf = [np.sign(val) for val in increase_or_decrease_for_counterfactual]
         third_array_values = ['Increased Value' if val < 0 else 'Decreased Value' if val > 0 else 'No Change' for val in result_increase_cf]
@@ -146,9 +144,8 @@ class CounterfactualIG(ExplainerBase):
 
 
         fig, ax = plt.subplots(figsize=(10, 6))
-        bars =[]
-        # Plot the horizontal bars
         bars = []
+        # Plot the horizontal bars
         for idx, value in enumerate(explanation):
             color = 'red' if value < 0 else 'green'
             bar = ax.barh(feature_names[idx], value, color=color)
@@ -157,9 +154,15 @@ class CounterfactualIG(ExplainerBase):
         # Attach data as annotations to each bar
             mplcursors.cursor(bar).connect(
                 "add", lambda sel, idx=idx: sel.annotation.set_text(
-                    f'Old Feature Value: {input_tensor[2:3].detach().numpy()[0][idx]}, New Feature Value: {nearest_neighbour_list[2].detach().numpy()[idx]}'
+                    f'Old Feature Value: {input_tensor[self.explain_index:self.explain_index+1].detach().numpy()[idx]}, New Feature Value: {nearest_neighbour_list[self.explain_index].detach().numpy()[idx]}'
                 )
             )
+        cursor = mplcursors.cursor(bars, hover=True)
+        true_class =  np.round(self.model_interface(query_instance).detach().numpy())[target]
+        counterfactual_class = np.round(self.model_interface([counterfactual][0]).detach().numpy())[target]
+        top_right_text = f'True Class: {true_class} \n Counterfactual Class: {counterfactual_class}'
+        ax.text(0.35, 0.95, top_right_text, transform=ax.transAxes,
+            fontsize=12, fontweight='bold', ha='right')
     
         ax.set_xlabel('Attribution Values')    
     
@@ -171,7 +174,7 @@ class CounterfactualIG(ExplainerBase):
         #    sel.annotation.get_bbox_patch().set(fc="white", alpha=0.8)
 
         # Add the interactivity using mplcursors
-        cursor = mplcursors.cursor(bars, hover=True)
+       
         #cursor.connect("add", on_hover)
         # Set the x-axis label
         plt.title("Counterfactual Feature Attribution")
